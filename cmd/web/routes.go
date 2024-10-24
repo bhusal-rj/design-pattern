397898cd909d983b6c51679f566cf26906606238
Rajesh <tipstricks118@gmail.com>
2024-10-24 19:58:38 +0545

    Project setup

---
+++ cmd/web/routes.go
@@ -0,0 +1,21 @@
+package main
+
+import (
+	"net/http"
+	"time"
+
+	"github.com/go-chi/chi/v5"
+	"github.com/go-chi/chi/v5/middleware"
+)
+
+func (app *application) routes() http.Handler {
+	mux := chi.NewRouter()
+	//show the 404 route if the non of the routes get matched.
+	mux.Use(middleware.Recoverer)
+
+	//timeout the request if it takes more than 60 seconds to complete.
+	mux.Use(middleware.Timeout(60 * time.Second))
+
+	mux.Get("/", app.ShowHome)
+	return mux
+}
---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/bhusal-rj/design-pattern
+
+go 1.23.2
+
+require github.com/go-chi/chi/v5 v5.1.0 // indirect
